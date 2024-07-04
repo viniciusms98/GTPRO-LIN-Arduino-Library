e68b708a969e67d397e415a3d5fdc922d48ae3e9
Vinicius Moreira <viniciusmoreirasouza@gmail.com>
2024-07-04 16:09:34 -0300

    Only library files

--- gtpro_lin.cpp
+++ gtpro_lin.cpp
@@ -9,9 +9,45 @@ comunicacaoLin::comunicacaoLin(uint8_t pin_tx, uint8_t pin_rx, uint8_t pin_desab
     this->tx_speed = tx_speed;
     this->id_esp = id_esp;
     this->id_tx = id_tx;
+    pinMode(pin_desable, OUTPUT); // Define o pin_desable como saída
+    Serial1.begin(tx_speed);      // Inicia o Serial1 na velocidade definida em tx_speed
 }
 
-uint8_t comunicacaoLin::function_sum(uint8_t *data, uint8_t length)
+// Função de  que executa as funções de recepção dos dados
+void comunicacaoLin::rxLoop()
+{
+    if (!Serial1.available()) // Inicia a configuração de recepção dos dados e verifica se houve erro
+    {
+        return; // Em caso de erro sai da função
+    }
+
+    uint8_t *data_received = new uint8_t[15]; // Criação de Ponteiro de Dados
+
+    Serial1.readBytes(data_received, 15); // Armazena os dados recebidos no ponteiro data_received
+    if (data_received[0] != id_tx)
+    {
+        delete[] data_received;
+        return;
+    }
+
+    data_received[0] = 0;
+
+    uint8_t checkSumRx = data_received[14];
+    data_received[14] = 0;
+
+    uint8_t sum = checkSumCalc(data_received, 14); // Calculando a soma dos dado de informação
+
+    if (checkSumRx == sum) // Verifica se os identificadores e soma estão corretos
+    {
+        received_function(data_received); // Se ocorrer tudo corretamente, retorna ponteiro de dados de informação
+    }
+
+    delete[] data_received;
+}
+
+// Funções de Recepção
+
+uint8_t comunicacaoLin::checkSumCalc(uint8_t *data, uint8_t length)
 {
     uint16_t sum = 0;
     for (uint8_t i = 0; i < length; i++)
@@ -23,101 +59,24 @@ uint8_t comunicacaoLin::function_sum(uint8_t *data, uint8_t length)
             sum -= 0xFF;
         }
     }
-
     // O checksum é o complemento de dois da soma
     return ~sum & 0xFF;
 }
 
-uint8_t comunicacaoLin::function_dataSeparation(uint8_t *data, uint8_t length)
-{
-
-    uint8_t data_received[length-3];
-
-    for (uint8_t i = 0; i < length-3; i++)
-    {
-        data_received[i] = data[i+2];
-    }
-    return data_received[12];
-}
-
-uint8_t* comunicacaoLin::function_concatenateData(uint8_t id_esp, uint8_t id_tx, uint8_t *data){
-
-    uint8_t concatenatedData [15];
-    concatenatedData[0] = id_esp;
-    concatenatedData[1] = id_tx;
-
-    for (uint8_t i = 2; i < 12; i++){
-        concatenatedData[i] = data[i-2];
-    }
-    concatenatedData[14] = function_sum(data, 12);
-    concatenatedData[15] = 0;
-    
-
-    return concatenatedData;
-}
-// - Inicialização da recepção
-void comunicacaoLin::begin()
-{
-    Serial1.begin(tx_speed);
-
-    pinMode(pin_desable, OUTPUT);
-}
-
-uint8_t comunicacaoLin::send(uint8_t *data, uint8_t data_length)
-{   
-    digitalWrite(pin_desable, LOW);
-    Serial1.write(data, data_length);
-    digitalWrite(pin_desable, HIGH);
-    return 0;
-}
-
-// Definição da função de loop
 void comunicacaoLin::setLoopFunction(void (*received_function)(uint8_t *data_received))
 {
     this->received_function = received_function;
 }
 
-uint8_t comunicacaoLin::loop(void (*received_function)(uint8_t *data_received))
+// Função de  que executa as funções de envio dos dados
+void comunicacaoLin::send(uint8_t *data, uint8_t id_tx = 0)
 {
-    if (this->received_function != nullptr)
-    {
-        this->received_function = received_function;
-    }
-    if (this->received_function == nullptr)
-    {
-        return 1; // Erro - No function received
-    }
-
-    if (Serial1.available())
-    {
-        uint8_t *data_received;
-
-        data_received = new uint8_t[15];
-
-        Serial1.readBytes(data_received, 15);
-
-        if (data_received[14] != 0 && data_received[0] == id_tx)
-        {
-            uint8_t dataSepareted = function_dataSeparation(data_received, 15);
-            uint8_t checksum = function_sum(&dataSepareted, 12);
-
-            if (data_received[1] == id_esp && data_received[1] == id_tx && checksum == data_received[14])
-            {
-                this->received_function(data_received);
-                send(data_received, 15);
-
-                return 0;
-            }
-            else
-            {
-                return 1;
-            }
-        }
-        else
-        {
-            return 1;
-        }
-
-        return 0;
-    }
+    digitalWrite(pin_desable, LOW);
+    if (id_tx)
+        Serial1.write(id_tx);
+    else
+        Serial1.write(this-> id_tx);
+    Serial1.write(data, 13);
+    Serial1.write(checkSumCalc(data, 12));
+    digitalWrite(pin_desable, HIGH);
 }
--- gtpro_lin.hpp
+++ gtpro_lin.hpp
@@ -6,17 +6,12 @@ class comunicacaoLin
 {
 public:
     comunicacaoLin(uint8_t pin_tx, uint8_t pin_rx, uint8_t pin_desable, uint8_t tx_speed, uint8_t id_esp, uint8_t id_tx = 0x04);
-    
-    void begin();
-    void setLoopFunction(void (*received_function)(uint8_t *data_received));
+    void
+        rxLoop(),
+        setLoopFunction(void (*received_function)(uint8_t *data_received)),
+        send(uint8_t *data, uint8_t id_tx = 0);   
+    uint8_t checkSumCalc(uint8_t *data, uint8_t length);
 
-    uint8_t loop(void (*received_function)(uint8_t *data_received)),
-        send(uint8_t *data, uint8_t data_length),
-        function_sum(uint8_t *data, uint8_t length),
-        function_dataSeparation(uint8_t *data, uint8_t length);
-
-    uint8_t *function_concatenateData(uint8_t id_esp, uint8_t id_tx, uint8_t *data);
-    
 private:
     uint8_t
         pin_tx,
@@ -25,7 +20,7 @@ private:
         tx_speed,
         id_esp,
         id_tx;
-        
+
     void (*received_function)(uint8_t *data_received) = nullptr;
 };
 
